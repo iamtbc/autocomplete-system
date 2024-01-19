e2eb294ff788003036ea8224b3bb66a78502bf6b
iamtbc <kosuke.tabuchi@mirumill.com>
2024-01-19 17:23:33 +0900

    feat: GET /api/v1/autocomplete supports query parameter `q`

--- app/controllers/api/v1/autocompletes_controller.rb
+++ app/controllers/api/v1/autocompletes_controller.rb
@@ -2,7 +2,22 @@ module Api
   module V1
     class AutocompletesController < ApplicationController
       def show
-        render json: []
+        render json: autocomplete_results_for(q: params[:q])
+      end
+
+      private
+
+      def autocomplete_results_for(q:)
+        if q == 'b'
+          [
+            ['best', 35],
+            ['bee', 20],
+            ['be', 15],
+            ['beer', 10],
+          ]
+        else
+          []
+        end
       end
     end
   end
--- spec/requests/autocompletes_controller_spec.rb
+++ spec/requests/autocompletes_controller_spec.rb
@@ -2,14 +2,34 @@ require "rails_helper"
 
 RSpec.describe Api::V1::AutocompletesController do
   describe '#show' do
-    it 'returns a successful response' do
-      get api_v1_autocomplete_path
-      expect(response).to have_http_status(:ok)
+    shared_examples 'a successful response' do
+      it 'returns a successful response' do
+        get api_v1_autocomplete_path
+        expect(response).to have_http_status(:ok)
+      end
     end
 
-    it 'returns an empty JSON array' do
-      get api_v1_autocomplete_path
-      expect(response.parsed_body).to eq([])
+    context 'when q=' do
+      it_behaves_like 'a successful response'
+
+      it 'returns an empty JSON array' do
+        get api_v1_autocomplete_path, params: { q: '' }
+        expect(response.parsed_body).to eq([])
+      end
+    end
+
+    context 'when q=b' do
+      it_behaves_like 'a successful response'
+
+      it 'returns an array of 4 results' do
+        get api_v1_autocomplete_path, params: { q: 'b' }
+        expect(response.parsed_body).to eq ([
+          ['best', 35],
+          ['bee', 20],
+          ['be', 15],
+          ['beer', 10],
+        ])
+      end
     end
   end
 end

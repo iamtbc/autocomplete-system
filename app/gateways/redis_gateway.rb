083a458a814875616014d32ef6c9dc4b57d8b799
iamtbc <kosuke.tabuchi@mirumill.com>
2024-01-23 09:53:32 +0900

    feat: add RedisGateway

---
+++ app/gateways/redis_gateway.rb
@@ -0,0 +1,15 @@
+class RedisGateway
+  def initialize(host:, port:, db:)
+    @host = host
+    @port = port
+    @db = db
+  end
+
+  delegate :get, :set, to: :client
+
+  private
+
+  def client
+    @client ||= Redis.new(host: @host, port: @port, db: @db)
+  end
+end
---
+++ spec/gateways/redis_gateway_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+RSpec.describe RedisGateway do
+  let(:host) { "localhost" }
+  let(:port) { 6379 }
+  let(:db) { 9 }
+  let(:gateway) { RedisGateway.new(host: host, port: port, db: db) }
+
+  let(:redis) { instance_spy(Redis) }
+  before do
+    allow(Redis).to receive(:new).and_return(redis)
+  end
+
+  describe "#get" do
+    it "calls Redis#get" do
+      gateway.get('test_key')
+      expect(redis).to have_received(:get).with('test_key')
+    end
+  end
+
+  describe "#set" do
+    it "calls Redis#set" do
+      gateway.set('test_key', 'test_value')
+      expect(redis).to have_received(:set).with('test_key', 'test_value')
+    end
+  end
+end

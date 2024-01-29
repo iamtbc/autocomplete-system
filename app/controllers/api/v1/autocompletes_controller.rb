d126213e67faba643a7825d7fc9f272f068df4a7
iamtbc <kosuke.tabuchi@mirumill.com>
2024-01-29 23:46:25 +0900

    feat: Api::V1::AutocompletesController#show SearchServiceを組み込み

--- app/controllers/api/v1/autocompletes_controller.rb
+++ app/controllers/api/v1/autocompletes_controller.rb
@@ -2,22 +2,26 @@ module Api
   module V1
     class AutocompletesController < ApplicationController
       def show
-        render json: autocomplete_results_for(q: params[:q])
+        autocompletes = search_service.search(q: params[:q])
+
+        render json: autocompletes
       end
 
       private
 
-      def autocomplete_results_for(q:)
-        if q == 'b'
-          [
-            ['best', 35],
-            ['bee', 20],
-            ['be', 15],
-            ['beer', 10],
-          ]
-        else
-          []
-        end
+      def search_service
+        @search_service ||= Autocompletes::Searcher.new(cache_service:)
+      end
+
+      def cache_service
+        @cache_service ||= Autocompletes::Cacher.new(cache_gateway:)
+      end
+
+      def cache_gateway
+        host = ENV.fetch("AUTOCOMPLETE_SYSTEM_CACHE_HOST")
+        port = ENV.fetch("AUTOCOMPLETE_SYSTEM_CACHE_PORT")
+        db = ENV.fetch("AUTOCOMPLETE_SYSTEM_CACHE_DB")
+        @cache_gateway ||= RedisGateway.new(host:, port:, db:)
       end
     end
   end
--- spec/requests/autocompletes_controller_spec.rb
+++ spec/requests/autocompletes_controller_spec.rb
@@ -1,7 +1,29 @@
 require "rails_helper"
 
+FIXTURE_FILE_PATH = 'spec/fixtures/files/autocompletes.json'
+
 RSpec.describe Api::V1::AutocompletesController do
   describe '#show' do
+    let(:host) { "localhost" }
+    let(:port) { 6379 }
+    let(:db) { 9 }
+    let(:redis_gateway) { RedisGateway.new(host: host, port: port, db: db) }
+    let(:cache_service) { Autocompletes::Cacher.new(cache_gateway: redis_gateway) }
+
+    before do
+      cache_service.import_from_json_file(file_path: FIXTURE_FILE_PATH)
+    end
+
+    after do
+      redis_gateway.flushdb
+    end
+
+    before do
+      allow(ENV).to receive(:fetch).with("AUTOCOMPLETE_SYSTEM_CACHE_HOST").and_return(host)
+      allow(ENV).to receive(:fetch).with("AUTOCOMPLETE_SYSTEM_CACHE_PORT").and_return(port)
+      allow(ENV).to receive(:fetch).with("AUTOCOMPLETE_SYSTEM_CACHE_DB").and_return(db)
+    end
+
     shared_examples 'a successful response' do
       it 'returns a successful response' do
         get api_v1_autocomplete_path
@@ -24,10 +46,10 @@ RSpec.describe Api::V1::AutocompletesController do
       it 'returns an array of 4 results' do
         get api_v1_autocomplete_path, params: { q: 'b' }
         expect(response.parsed_body).to eq ([
-          ['best', 35],
-          ['bee', 20],
-          ['be', 15],
-          ['beer', 10],
+          [ 'best', 35 ],
+          [ 'bee', 20 ],
+          [ 'be', 15 ],
+          [ 'beer', 10 ]
         ])
       end
     end
